e170003ce0dd1106a84c0e8a5289c05600b58ebb
crispy <brian3092@naver.com>
2026-06-17 11:38:19 +0900

    065

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ repositories {
 
 dependencies {
     dependencies {
+        implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+        runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+        runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+
         implementation("org.springframework.boot:spring-boot-starter-validation")
 
         implementation("org.springframework.boot:spring-boot-starter-web")
